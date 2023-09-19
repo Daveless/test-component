666ac8ad5ff10a3406dcdaa46308bda684b9f49f
David <davidalrojas@gmail.com>
2023-09-19 16:32:04 -0500

    first commit and mobile view completed

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Brands from "./components/Brands";
+import Footer from "./components/Footer";
+import Guarantee from "./components/Guarantee";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <>
+      <div className="w-full flex flex-col justify-center items-center gap-[2rem]">
+        <header className="flex flex-col items-center gap-[1.5rem] w-full">
+          <Guarantee />
+          <Brands />
+        </header>
+        <Main />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
